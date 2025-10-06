76f4d809d92527272cef337224c3eb62b3f01f8d
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-10-06 12:12:02 +0100

    tried adding tiktok fonts, don't know if it worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import liveIcon from "./assets/tiktok icons/tiktok-live.svg";
 function App() {
   return (
-    <div className="min-h-screen min-w-screen">
-      <h1>Tiktok time</h1>
+    <div className="min-h-screen min-w-screen bg-black/40 p-4">
+      <div>
+        <div>
+          <img src={liveIcon} alt="tiktok live icon" width={35} />
+        </div>
+        <ul>
+          <li>STEM</li>
+          <li>Explore</li>
+          <li>Following</li>
+          <li>For You</li>
+        </ul>
+      </div>
     </div>
   );
 }

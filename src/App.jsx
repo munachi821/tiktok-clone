af2f34125a162f1955fc922ba27166cccb87fb60
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-10-06 16:06:07 +0100

    done with the user interactions section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
 import liveIcon from "./assets/tiktok icons/tiktok-live.svg";
 import searchIcon from "./assets/tiktok icons/tiktok-search.svg";
+import heartIcon from "./assets/tiktok icons/tiktok-heart.svg";
+import commentIcon from "./assets/tiktok icons/tiktok-comments.svg";
+import favoriteIcon from "./assets/tiktok icons/tiktok-favorite.svg";
+import forwardIcon from "./assets/tiktok icons/tiktok-forward.svg";
+import { Plus } from "lucide-react";
 function App() {
   return (
-    <div className="min-h-screen min-w-screen bg-black/40 p-4">
-      <div>
+    <div className="min-h-screen min-w-screen bg-red-400 p-4">
+      <div className="flex w-full justify-between items-center">
         <div>
           <img src={liveIcon} alt="tiktok live icon" width={35} />
         </div>
-        <ul className="inline-flex">
-          <li>STEM</li>
-          <li>Explore</li>
-          <li>Following</li>
-          <li>For You</li>
+        <ul className="inline-flex text-[#B8B9BD] gap-4 font-bold pt-5">
+          <li className="cursor-pointer">STEM</li>
+          <li className="cursor-pointer">Explore</li>
+          <li className="cursor-pointer">Following</li>
+          <li className="text-white border-b-2 pb-3 w-8 whitespace-nowrap flex items-center justify-center ml-4">
+            For You
+          </li>
         </ul>
         <div>
           <img src={searchIcon} alt="tiktok search icon" width={35} />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center w-fit gap-5">
+        <div className="size-16 rounded-full bg-black relative">
+          <div className="absolute size-6 rounded-full -bottom-2 left-5.5 flex items-center justify-center text-white bg-[#FD2C54]">
+            <Plus size={18} />
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img src={heartIcon} alt="tiktok heart icon" width={37} />
+          <span className="font-bold text-white text-xl">87.1k</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <img src={commentIcon} alt="tiktok comment icon" width={37} />
+          <span className="font-bold text-white text-xl">5,197</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <img src={favoriteIcon} alt="tiktok favorite icon" width={37} />
+          <span className="font-bold text-white text-xl">4,428</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <img src={forwardIcon} alt="tiktok forward icon" width={37} />
+          <span className="font-bold text-white text-xl">9,782</span>
         </div>
       </div>
     </div>

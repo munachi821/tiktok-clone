b6d425ff105d73bf8f321580884d0c7f49715b0a
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-10-06 15:16:06 +0100

    the svg had some issues so i fixed them in coreldraw

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import liveIcon from "./assets/tiktok icons/tiktok-live.svg";
+import searchIcon from "./assets/tiktok icons/tiktok-search.svg";
 function App() {
   return (
     <div className="min-h-screen min-w-screen bg-black/40 p-4">
@@ -6,12 +7,15 @@ function App() {
         <div>
           <img src={liveIcon} alt="tiktok live icon" width={35} />
         </div>
-        <ul>
+        <ul className="inline-flex">
           <li>STEM</li>
           <li>Explore</li>
           <li>Following</li>
           <li>For You</li>
         </ul>
+        <div>
+          <img src={searchIcon} alt="tiktok search icon" width={35} />
+        </div>
       </div>
     </div>
   );
